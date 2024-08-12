6fc7dd2100954bab16d82714a637e3429888fa15
guineaaaa <165776804+guineaaaa@users.noreply.github.com>
2024-08-12 23:43:10 +0900

    💄 UI 수정, CORS 에러..

--- TravelMate/vite.config.js
+++ TravelMate/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['@fortawesome/free-solid-svg-icons', '@fortawesome/free-brands-svg-icons'],
+   
   },
 })
